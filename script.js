5ea25fa9bf1f5d03086792330e3bb30c9bca48b7
TrungHieu310 <129833095+TrungHieu310@users.noreply.github.com>
2023-04-26 18:03:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 100) + 1;
 let score = 20;
 let highscore = 0;
 
